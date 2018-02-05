056cf3cac7685e6c25299267d042c60644463df0
Ivan Burmin <zirrald@yandex.ru>
2018-02-05 17:10:23 +0300

    Print class

---
+++ zir.util/src/com/zir/util/Print.java
@@ -0,0 +1,19 @@
+package com.zir.util;
+
+public class Print {
+    public static void ln() {
+        System.out.println();
+    }
+    public static void ln(Object obj) {
+        System.out.println(obj);
+    }
+    public static void ln(String str) {
+        System.out.println(str);
+    }
+    public static void lnn(Object obj) {
+        System.out.print(obj);
+    }
+    public static void lnn(String str) {
+        System.out.print(str);
+    }
+}
