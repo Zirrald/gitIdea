a1fae2ecf66cefc597e4a86d5457bb9bed71eb82
Ivan Burmin <zirrald@yandex.ru>
2018-02-05 18:30:30 +0300

    Code style

--- zir.core/src/com/zir/core/Main.java
+++ zir.core/src/com/zir/core/Main.java
@@ -6,6 +6,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello", "Yahoo!", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(
+                null,
+                "Hello",
+                "Yahoo!",
+                JOptionPane.PLAIN_MESSAGE);
     }
 }
