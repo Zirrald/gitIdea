97c139384c090cf6db96a696d1c4d1ae69b56023
Ivan Burmin <zirrald@yandex.ru>
2018-02-07 16:57:26 +0300

    Modify for-loop statements

--- zir.core/src/com/zir/core/Calculate.java
+++ zir.core/src/com/zir/core/Calculate.java
@@ -30,19 +30,22 @@ public class Calculate {
 
         /* Calculate stack length */
         int tempVal = number;
-        for (stackLength=0; tempVal != 0; stackLength++) {
-            tempVal /= 2;
-        }
+
+        for (stackLength=0; tempVal != 0; tempVal /= 2, stackLength++);
+//        for (stackLength=0; tempVal != 0; stackLength++) {
+//            tempVal /= 2;
+//        }
 
         /* Register stack */
         Stack stack = new Stack(stackLength);
 
         /* Fill up stack */
-        for (; number != 0;) {
-            reminder = number % 2;
-            stack.addValue(reminder);
-            number /= 2;
-        }
+        for (; number != 0; stack.addValue(number % 2), number /= 2);
+//        for (; number != 0;) {
+//            reminder = number % 2;
+//            stack.addValue(reminder);
+//            number /= 2;
+//        }
 
         /* Form output */
         for (int i=0, counter = 0; i < stackLength; i++) {
